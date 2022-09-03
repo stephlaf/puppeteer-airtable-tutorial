1ac9f42ae04b8fdac1983b23ee9905cbdba93607
Stephane Lafontaine <stephane@medqigong.ca>
2022-09-03 17:32:47 -0400

    Refactored to arrow functions

--- dieuduciel.js
+++ dieuduciel.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 const puppeteer = require("puppeteer");
 
 const Airtable = require('airtable');
-var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BOARD_KEY);
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BOARD_KEY);
 
-async function writeToAirtable(beerObjects) {
+const writeToAirtable = async (beerObjects) => {
   // Iterate over the Array & write the infos for each beer to the Airtable table
   beerObjects.forEach((beer) => {
-    base('Table 1').create(beer, function(err, record) {
+    base('Table 1').create(beer, (err, record) => {
       if (err) {
         console.error(err);
         return;
@@ -28,40 +28,41 @@ async function writeToAirtable(beerObjects) {
 
   // Once the page is loaded
   const beerObjects = await page.evaluate(async () => {
+    // console.log("Let's scrape this page! 😃");
     // Create an Array to store each beer Object
     const beersArray = [];
 
     // Get all the links from the page & turn the NodeList into an Array
-    const elements = document.querySelectorAll('.bieres__biere a');
-    const beerLinks = Array.from(elements)
+    const beerLinks = document.querySelectorAll('.bieres__biere a');
+    const beerLinksArray = Array.from(beerLinks)
 
     // Loop over the Array
-    for (let link of beerLinks.slice(0, 2)) {
+    for (let link of beerLinksArray.slice(0, 2)) {
     // for (let link of beerLinks) {
 
       // click on the link
       await link.click();
 
-      /* Wait for network response */
+      // Wait for network response
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       // Scrape the infos
       const beer_infos = document.querySelector('.biere__header-container')
-      const imageUrl = beer_infos.querySelector('.biere__img img').src
-      const category = beer_infos.querySelector('h4.heading').innerText
       const name = beer_infos.querySelector('h2.biere__heading').innerText
+      const category = beer_infos.querySelector('h4.heading').innerText
       const type = beer_infos.querySelector('p > strong').innerText
       const alcohol = beer_infos.querySelectorAll('p')[1].innerText
+      const imageUrl = beer_infos.querySelector('.biere__img img').src
       const description = document.querySelectorAll('div.biere__description p')[1].innerText
 
       // Store the infos Object in the Array
       beersArray.push({
-        name: name,
-        description: description,
+        name,
+        category,
+        type,
+        alcohol,
         image_url: imageUrl,
-        category: category,
-        type: type,
-        alcohol: alcohol
+        description
       });
     }
     // Once the loop is over, return the Array
